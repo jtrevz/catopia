9334f4845178b2425299ebe3d2406bebbfb004c6
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-02-24 09:53:40 -0600

    cats typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,5 +4,18 @@ const typeDefs = gql`
     type Cat {
         _id: ID
         name: String
+        age: String
+        gender: String
+        coat: String
+        breed: String
+        color: String
+        houseTrained: String
+        spayNeutered: String
+        shots: String
+        description: String
+    }
+
+    type Query {
+    cats: [Cat]
     }
 `;
